f779632f381c1f01bec4105aefce55ab612f32fb
refond <refon@pobox.com>
2014-01-08 13:19:31 +0100

    fixing missing package level hyperbird. Adding more exclusion to gitignore.

---
+++ app/ch/bsisa/hyperbird/controllers/Application.scala
@@ -0,0 +1,12 @@
+package ch.bsisa.hyperbird.controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("HyperBird 5.0"))
+  }
+
+}
