3382767f1188092268f424ad8b3d77a0f0cb51d2
Patrick Refondini <patrick.refondini@pobox.com>
2014-01-15 22:58:45 +0100

    Start to implement simple user authentication support.

---
+++ app/ch/bsisa/hyperbird/security/Secured.scala
@@ -0,0 +1,29 @@
+package ch.bsisa.hyperbird.security
+
+import play.api.mvc._
+
+
+/**
+ * Provide security features.
+ *
+ * Security trait design after play.api.mvc.Security.Authenticated examples.
+ */
+trait Secured { self: Controller =>
+
+  val UserNameSessionToken = "user"
+  
+  /**
+   * Retrieve the connected user id.
+   */
+  def username(request: RequestHeader) = request.session.get(UserNameSessionToken)
+
+  /**
+   * Redirect to login if the use in not authorized.
+   */
+  def onUnauthorized(request: RequestHeader): SimpleResult
+
+  def IsAuthenticated(f: => String => Request[AnyContent] => Result) =
+    Security.Authenticated(username, onUnauthorized) { user =>
+      Action(request => f(user)(request))
+    }
+}
