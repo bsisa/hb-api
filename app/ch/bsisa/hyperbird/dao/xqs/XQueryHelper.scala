2e4657f0aabfe63fa9244f96478309fe4a87b452
refond <refon@pobox.com>
2014-01-20 23:04:18 +0100

    Use finally to make sure to close connection even if an exception occurs.

--- app/ch/bsisa/hyperbird/dao/xqs/XQueryHelper.scala
+++ app/ch/bsisa/hyperbird/dao/xqs/XQueryHelper.scala
@@ -8,42 +8,46 @@ import com.felstar.xqs.XQS
  */
 object XQueryHelper {
 
-    /**
+  /**
    * Encapsulate Exist connection, prepare expression and query execution.
    * Connection seems to be light weight... To be verified.
    */
   def seqOfElem(xquery: String): Seq[scala.xml.Elem] = {
 
     val conn = XQConnectionHelper.getConnection()
-    val xqpe = conn.prepareExpression(xquery)
-    val queryResult = xqpe.executeQuery()
+    try {
+      val xqpe = conn.prepareExpression(xquery)
+      val queryResult = xqpe.executeQuery()
 
-//    var serialisationProps = new java.util.Properties()
-//    serialisationProps.setProperty("method", "xml")
-//    serialisationProps.setProperty("indent", "yes")
-//    serialisationProps.setProperty("encoding", "ISO8859-1") // Default is UTF-8
-//    serialisationProps.setProperty("omit-xml-declartaion", "no")
-    
-//    println("DEBUG ======================================== ")
-//    println("DEBUG ================= ISO8859-1 ============ ")
-//    println("DEBUG ======================================== ")
-    //println(queryResult.toString())
-    
-//     while (queryResult.next()) {
-//       //XQItemType type
-//       val theType = queryResult.getItemType();
-//       println("Type: " + theType)
-//       //println("ATOMIC: " + queryResult.getAtomicValue())
-//       //println(queryResult.getItemAsString(null))
-//       println(queryResult.getItemAsString(serialisationProps))
-//     }
-   
-//    println("DEBUG ======================================== ")
-//    println("DEBUG ======================================== ")
-//    println("DEBUG ======================================== ")
-    val xmls: Seq[scala.xml.Elem] = XQS.toSeqXML(queryResult);
-    conn.close();
-    xmls
+      //    var serialisationProps = new java.util.Properties()
+      //    serialisationProps.setProperty("method", "xml")
+      //    serialisationProps.setProperty("indent", "yes")
+      //    serialisationProps.setProperty("encoding", "ISO8859-1") // Default is UTF-8
+      //    serialisationProps.setProperty("omit-xml-declartaion", "no")
+
+      //    println("DEBUG ======================================== ")
+      //    println("DEBUG ================= ISO8859-1 ============ ")
+      //    println("DEBUG ======================================== ")
+      //println(queryResult.toString())
+
+      //     while (queryResult.next()) {
+      //       //XQItemType type
+      //       val theType = queryResult.getItemType();
+      //       println("Type: " + theType)
+      //       //println("ATOMIC: " + queryResult.getAtomicValue())
+      //       //println(queryResult.getItemAsString(null))
+      //       println(queryResult.getItemAsString(serialisationProps))
+      //     }
+
+      //    println("DEBUG ======================================== ")
+      //    println("DEBUG ======================================== ")
+      //    println("DEBUG ======================================== ")
+      
+      val xmls: Seq[scala.xml.Elem] = XQS.toSeqXML(queryResult);
+      xmls
+    } finally {
+      conn.close();
+    }
   }
-  
+
 }
