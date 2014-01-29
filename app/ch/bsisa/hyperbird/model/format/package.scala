27a45150a6799fce85edf8b2140c30aec3d81a44
refond <refon@pobox.com>
2014-01-29 16:25:35 +0100

    Scaladoc improvement.

--- app/ch/bsisa/hyperbird/model/format/package.scala
+++ app/ch/bsisa/hyperbird/model/format/package.scala
@@ -5,12 +5,16 @@ package ch.bsisa.hyperbird.model
  * to and from miscellaneous serialisation formats, 
  * for instance JSON and XML.
  * 
- * These functions heavily depend on third party libraries such as 
- * Play Framework `play.api.libs.json` or Lift Framework `net.liftweb.json`
- * which must not be the case for model objects. It might be useful to 
- * package model objects and utilities to read and write them from 
- * miscellaneous serialisation formats separately depending on usage
- * and technologies evolutions.
+ * These functions heavily depend on third party libraries such as: 
+ - Play Framework `play.api.libs.json` 
+ - Lift Framework `net.liftweb.json`
+ * 
+ * which must not be the case for model objects. 
+ * 
+ * It might be useful to package model objects and utilities to read 
+ * and write them from miscellaneous serialisation formats separately 
+ * depending on usage and technologies evolutions.
+ * 
  * 
  * @author Patrick Refondini
  * 
