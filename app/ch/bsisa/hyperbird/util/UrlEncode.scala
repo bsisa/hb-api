e4d6de9d7a3c30e1101fe9f59e89578de982daf5
refond <refon@pobox.com>
2014-02-18 14:37:35 +0100

    Implement UrlEncode test to document usage.

--- app/ch/bsisa/hyperbird/util/UrlEncode.scala
+++ app/ch/bsisa/hyperbird/util/UrlEncode.scala
@@ -1,13 +1,20 @@
 package ch.bsisa.hyperbird.util
 
-object UrlEncode extends App {
-  
+/**
+ * URL/URI encode helper
+ *
+ * Note: java.net.URLEncoder.encode() is an option but it requires to separate
+ * individual query string parameter name and values from the rest of the URL.
+ * Indeed parameter separator character `&` or parameter name/value separator
+ * character `=` of the query string must not be escaped.
+ *
+ * @author Patrick Refondini
+ */
+object UrlEncode {
 
-  // TODO: make test usage with exist REST api
-  // TODO: implement unit tests
-  println("encodeURL : " + encodeURL(scheme = "http", host = "localhost", port = 9000, path = "/api/mel fin/G20040930101030005", query = "xpath=//ELFIN[@Id='G2004 0203114894000']"))
-  
-  
+  /**
+   * Use java.net.URI to URI encode using most detailed URI constructor.
+   */
   def encodeURL(scheme: String = "", userInfo: String = null, host: String = "localhost", port: Int = 80, path: String = null, query: String = null, fragment: String = null): String = {
     val uri = new java.net.URI(scheme, userInfo, host, port, path, query, fragment)
     uri.toASCIIString()
---
+++ test/test/ch/bsisa/hyperbird/util/UrlEncodeSpec.scala
@@ -0,0 +1,34 @@
+package test.ch.bsisa.hyperbird.util
+
+import ch.bsisa.hyperbird.util.UrlEncode
+
+/**
+ * Tests URL encoding
+ *
+ * @author Patrick Refondini
+ */
+class UrlEncodeSpec extends BaseSerialisationSpec {
+
+  //UrlEncode.encodeURL(scheme = "http", host = "localhost", port = 9000, path = "/api/mel fin/G20040930101030005", query = "xpath=//ELFIN[@Id='G2004 0203114894000']")
+
+  val originalURL_scheme_001 = "http"
+  val originalURL_host_001 = "localhost"
+  val originalURL_port_001 = 9000
+  val originalURL_path_001 = "/api/mel fin/G20040930101030005"
+  val originalURL_fragment_001 = "xpath=//ELFIN[@Id='G2004 0203114894000']"
+
+  val expectedURL_001 = "http://localhost:9000/api/mel%20fin/G20040930101030005?xpath=//ELFIN[@Id='G2004%200203114894000']"
+
+  s"The URL scheme ${originalURL_scheme_001}, host ${originalURL_host_001}, port ${originalURL_port_001}, path ${originalURL_scheme_001}, fragment ${originalURL_scheme_001} " should {
+    s"be encoded to ${expectedURL_001}" in {
+      val encodeTest_001 = UrlEncode.encodeURL(
+        scheme = originalURL_scheme_001,
+        host = originalURL_host_001,
+        port = originalURL_port_001,
+        path = originalURL_path_001,
+        query = originalURL_fragment_001)
+      encodeTest_001 must be equalTo (expectedURL_001)
+    }
+  }
+
+}
