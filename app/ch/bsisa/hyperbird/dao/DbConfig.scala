0bcc82d29aa92f2f489f4450a7184def77abde87
refond <refon@pobox.com>
2014-03-05 19:49:01 +0100

    Best practise using companion object containing constants as done for collectionConfig.

--- app/ch/bsisa/hyperbird/dao/DbConfig.scala
+++ app/ch/bsisa/hyperbird/dao/DbConfig.scala
@@ -14,51 +14,51 @@ class DbConfig {
   /**
    * Database protocol
    */
-  val protocol: String = Play.current.configuration.getString("hb.db.protocol") match {
+  val protocol: String = Play.current.configuration.getString(DbConfig.DbProtocolKey) match {
     case Some(dbProtocol) => dbProtocol
-    case None => throw DbConfigException("Database configuration hb.db.protocol missing")
+    case None => throw DbConfigException(s"Database configuration ${DbConfig.DbProtocolKey} missing")
   }  
   
   /**
    * Database host name
    */
-  val hostName: String = Play.current.configuration.getString("hb.db.hostName") match {
+  val hostName: String = Play.current.configuration.getString(DbConfig.DbHostNameKey) match {
     case Some(dbHostName) => dbHostName
-    case None => throw DbConfigException("Database configuration hb.db.hostName missing")
+    case None => throw DbConfigException(s"Database configuration ${DbConfig.DbHostNameKey} missing")
   }
 
   /**
    * Database port
    */
-  val port: Integer = Play.current.configuration.getInt("hb.db.port") match {
+  val port: Integer = Play.current.configuration.getInt(DbConfig.DbPortKey) match {
     case Some(dbPort) => dbPort
-    case None => throw DbConfigException("Database configuration hb.db.port missing")
+    case None => throw DbConfigException(s"Database configuration ${DbConfig.DbPortKey} missing")
   }
   
   /**
    * Database REST service prefix 
    */
-  val restPrefix: String = Play.current.configuration.getString("hb.db.restPrefix") match {
+  val restPrefix: String = Play.current.configuration.getString(DbConfig.DbRestPrefixKey) match {
     case Some(dbRestPrefix) => dbRestPrefix
-    case None => throw DbConfigException("Database configuration hb.db.restPrefix missing")
+    case None => throw DbConfigException(s"Database configuration ${DbConfig.DbRestPrefixKey} missing")
   }
   
   /**
    * Database name
    */
-  val databaseName: String = Play.current.configuration.getString("hb.db.name") match {
+  val databaseName: String = Play.current.configuration.getString(DbConfig.DbNameKey) match {
     case Some(dbName) => dbName
-    case None => throw DbConfigException("Database configuration hb.db.name missing")
+    case None => throw DbConfigException(s"Database configuration ${DbConfig.DbNameKey} missing")
   }
   
-  val userName: String = Play.current.configuration.getString("hb.db.user") match {
+  val userName: String = Play.current.configuration.getString(DbConfig.DbUserKey) match {
     case Some(dbUserName) => dbUserName
-    case None => throw DbConfigException("Database configuration hb.db.user missing")
+    case None => throw DbConfigException(s"Database configuration ${DbConfig.DbUserKey} missing")
   }
   
-  val password: String = Play.current.configuration.getString("hb.db.password") match {
+  val password: String = Play.current.configuration.getString(DbConfig.DbPasswordKey) match {
     case Some(dbPassword) => dbPassword
-    case None => throw DbConfigException("Database configuration hb.db.password missing")
+    case None => throw DbConfigException(s"Database configuration ${DbConfig.DbPasswordKey} missing")
   }
 
 }
@@ -67,3 +67,19 @@ class DbConfig {
  * Database configuration exception class
  */
 case class DbConfigException(message: String = null, cause: Throwable = null) extends Exception(message, cause)
+
+
+/**
+ * Companion object containing constants
+ */
+object DbConfig {
+
+  private val DbProtocolKey = "hb.db.protocol"
+  private val DbHostNameKey = "hb.db.hostName"
+  private val DbPortKey = "hb.db.port"
+  private val DbRestPrefixKey = "hb.db.restPrefix"
+  private val DbNameKey = "hb.db.name"
+  private val DbUserKey = "hb.db.user"
+  private val DbPasswordKey = "hb.db.password"
+
+}
