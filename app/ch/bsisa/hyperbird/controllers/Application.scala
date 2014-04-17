5b22ab859a543e3f48f48508f84ab6de1df5e93f
refond <refon@pobox.com>
2014-04-17 20:26:54 +0200

    HTML5 history API support, aka AngularJs html5mode. Allow clean URL such as: / instead of /index.html#/ or: /elfin/G20040930101030005/IMMEUBLE/G20051121083149428 instead of: /index.html#/elfin/G20040930101030005/IMMEUBLE/G20051121083149428

--- app/ch/bsisa/hyperbird/controllers/Application.scala
+++ app/ch/bsisa/hyperbird/controllers/Application.scala
@@ -2,9 +2,9 @@ package ch.bsisa.hyperbird.controllers
 
 import ch.bsisa.hyperbird.Implicits._
 import ch.bsisa.hyperbird.ApiConfig
-
 import play.api._
 import play.api.mvc._
+import controllers.Assets
 
 /**
  * Application controller.
@@ -17,11 +17,13 @@ object Application extends Controller {
 
   def getApiBaseUrl()(implicit apiConfig: ApiConfig) =  apiConfig.baseUrl  
   
-  
-  def angularJsIndex = Action {
-    Redirect("/index.html")
-  }
-  
+  /**
+   * Added for HTML5 history API support, aka AngularJs html5mode.
+   * We need calling AngularJs index from custom controller to preserve full 
+   * deep URL while redirecting single page application landing page index.html    
+   */
+  def angularJsIndex(any: String) = Assets.at(path = "/public", file = "index.html") 
+
   def index = Action {
     Ok(views.html.index("HyperBird 5.0", ""))
   }
