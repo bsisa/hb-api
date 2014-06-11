3aa277e50644b0e031531e9e486d74ecf12008de
refond <refon@pobox.com>
2014-06-11 14:12:23 +0200

    Added Scaladoc, made logs more visible.

--- app/ch/bsisa/hyperbird/security/SecureServiceEventListener.scala
+++ app/ch/bsisa/hyperbird/security/SecureServiceEventListener.scala
@@ -1,16 +1,20 @@
 package ch.bsisa.hyperbird.security
 
-import securesocial.core._
+import securesocial.core.{Event,EventListener,LoginEvent,LogoutEvent,PasswordChangeEvent,PasswordResetEvent,SignUpEvent}
 import play.api.mvc.{Session, RequestHeader}
 import play.api.{Application, Logger}
 
 /**
  * SecureSocial service event listener.
  * 
+ * This listener can be enabled, disabled in conf/play.plugins as: 
+ * `10100:ch.bsisa.hyperbird.security.SecureServiceEventListener`
+ * Where 10100 is a priority number that determines the order in which plugins start up.
+ * 
  * @author Patrick Refondini
  */
 class SecureServiceEventListener(app: Application) extends EventListener {
-  override def id: String = "my_event_listener"
+  override def id: String = "hbSecureServiceEventListenerId"
    
   def onEvent(event: Event, request: RequestHeader, session: Session): Option[Session] = {
     val eventName = event match {
@@ -21,8 +25,8 @@ class SecureServiceEventListener(app: Application) extends EventListener {
       case e: PasswordChangeEvent => "password change"
     }
 
-    Logger.info("traced %s event for user %s".format(eventName, event.user.fullName))
-    Logger.debug(s"request.path = ${request.path} , request.rawQueryString = ${request.rawQueryString}")
+    Logger.info(">>>> SECURITY: traced %s event for user %s".format(eventName, event.user.fullName))
+    Logger.debug(s">>>> SECURITY: request.path = ${request.path} , request.rawQueryString = ${request.rawQueryString}")
     // Not changing the session so just return None
     // if you wanted to change the session then you'd do something like
     // Some(session + ("your_key" -> "your_value"))
