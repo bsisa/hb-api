8be5f3263aa5f7f9868e9e6218f479ebef8b1a7d
refond <refon@pobox.com>
2014-06-11 13:45:35 +0200

    Imports clean up. No code change.

--- app/ch/bsisa/hyperbird/controllers/Api.scala
+++ app/ch/bsisa/hyperbird/controllers/Api.scala
@@ -1,43 +1,40 @@
 package ch.bsisa.hyperbird.controllers
 
+import ch.bsisa.hyperbird.CollectionsConfig
+import ch.bsisa.hyperbird.InitConfig
+import ch.bsisa.hyperbird.Implicits._
+import ch.bsisa.hyperbird.dao.ws.WSQueries
+import ch.bsisa.hyperbird.dao.ws.XQueryWSHelper
+import ch.bsisa.hyperbird.dao.ElfinDAO
+import ch.bsisa.hyperbird.dao.ResultNotFoundException
+import ch.bsisa.hyperbird.model.ELFIN
+import ch.bsisa.hyperbird.model.format.ElfinFormat
+import ch.bsisa.hyperbird.spreadsheet.SpreadSheetBuilder
+import ch.bsisa.hyperbird.util.ElfinUtil
+
+import org.apache.poi.ss.usermodel._
+
+import securesocial.core.java.SecureSocial.SecuredAction
+
 import play.api._
 import play.api.Logger
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.json.Json
 import play.api.libs.ws.Response
 import play.api.libs.ws.WS
 import play.api.mvc._
+
+import scala.concurrent.Await
 import scala.concurrent.Future
 import scala.xml.XML
-import ch.bsisa.hyperbird.Implicits._
-import ch.bsisa.hyperbird.util.format.JsonXmlConverter
-import ch.bsisa.hyperbird.dao.ws.WSQueries
-import ch.bsisa.hyperbird.dao.ws.XQueryWSHelper
-import ch.bsisa.hyperbird.dao.xqs.XQSQueriesProcessor
-import ch.bsisa.hyperbird.dao.xqs.XQSQueries
-import ch.bsisa.hyperbird.dao.ElfinDAO
-import ch.bsisa.hyperbird.model.format.ElfinFormat
-import ch.bsisa.hyperbird.util.ElfinIdGenerator
-import ch.bsisa.hyperbird.model.ELFIN
-import play.api.libs.json.Json
-import ch.bsisa.hyperbird.util.ElfinUtil
-import ch.bsisa.hyperbird.InitConfig
-import ch.bsisa.hyperbird.CollectionsConfig
-import securesocial.core.java.SecureSocial.SecuredAction
-import ch.bsisa.hyperbird.dao.ResultNotFoundException
+
 import java.net.ConnectException
 import java.io.InputStream
-import play.api.libs.iteratee.Enumerator
-import org.apache.poi.ss.usermodel._
 import java.io.OutputStream
 import java.io.ByteArrayOutputStream
 import java.io.ByteArrayInputStream
-import org.apache.poi.ss.util.CellReference
-import scala.concurrent.Await
-import org.jsoup.Jsoup
 
-import ch.bsisa.hyperbird.spreadsheet.SpreadSheetBuilder
-
-//import org.apache.poi.ss.usermodel.{WorkbookFactory,Workbook,Sheet,Row,Cell}
 
 /**
  * REST API controller.
