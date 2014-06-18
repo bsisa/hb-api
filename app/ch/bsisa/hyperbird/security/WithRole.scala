fc83759a9962f5d34f21d9a27d27eb8f2b4a8ab2
refond <refon@pobox.com>
2014-06-18 16:19:11 +0200

    Role comment.

--- app/ch/bsisa/hyperbird/security/WithRole.scala
+++ app/ch/bsisa/hyperbird/security/WithRole.scala
@@ -5,19 +5,17 @@ import ch.bsisa.hyperbird.CollectionsConfig
 import ch.bsisa.hyperbird.dao.DbConfig
 import ch.bsisa.hyperbird.dao.ElfinDAO
 import ch.bsisa.hyperbird.model.ELFIN
-
 import securesocial.core.Authorization
 import securesocial.core.Identity
-
 import play.api.Logger
 import play.api.libs.concurrent.Execution.Implicits._
-
 import scala.concurrent.Future
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.concurrent.duration.MILLISECONDS
-
 import java.util.Date
+import ch.bsisa.hyperbird.model.CARACTERISTIQUE
+import ch.bsisa.hyperbird.model.MATRICEType
 
 case class WithRole(role: String) extends Authorization {
 
@@ -28,6 +26,14 @@ case class WithRole(role: String) extends Authorization {
     // Wait for the result of the last future layer
     val elfinUser = Await.result[ELFIN](futureElfinUser, Duration(8000, MILLISECONDS))
 
+//    val elfinUser.CARACTERISTIQUE match {
+//      case Some(CARACTERISTIQUE(_,_,_,_,_,_,_,_,_,fraction)) => fraction match {
+//        case Some(MATRICEType(lines)) => lines.
+//        case None => None
+//      }
+//      case None => None
+//    }
+    
     Logger.debug(s"WithRole(${role}) called at ${new Date} and actual user.role = ..." )
     Logger.debug(s"elfinUser.IDENTIFIANT.get.NOM.get = ${elfinUser.IDENTIFIANT.get.NOM.get}" )
     
