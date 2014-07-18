567d090739023d7b720cb317f74a6f44cdd23427
refond <refon@pobox.com>
2014-07-18 20:52:45 +0200

    Formatting.

--- app/ch/bsisa/hyperbird/ApiConfig.scala
+++ app/ch/bsisa/hyperbird/ApiConfig.scala
@@ -4,7 +4,7 @@ import play.api.Play
 
 /**
  * Api configuration object.
- * 
+ *
  * Provides JavaScript client with dynamic configurations
  *
  * @author Patrick Refondini
@@ -18,22 +18,20 @@ class ApiConfig {
     case Some(baseUrlValue) => baseUrlValue
     case None => throw ApiConfigException(s"ApiConfig base URL information ${ApiConfig.BaseUrlKey} missing")
   }
-  
+
   /**
    *  Used for ELFIN Annexes flat file storage
    */
   val annexesRootFolder: String = Play.current.configuration.getString(ApiConfig.AnnexesRootFolderKey) match {
     case Some(annexesRootFolderValue) => annexesRootFolderValue
     case None => throw ApiConfigException(s"ApiConfig annexes root folder information ${ApiConfig.AnnexesRootFolderKey} missing")
-  }  
+  }
 
-  
-    val temporaryUploadFolder: String = Play.current.configuration.getString(ApiConfig.TemporaryUploadFolderKey) match {
+  val temporaryUploadFolder: String = Play.current.configuration.getString(ApiConfig.TemporaryUploadFolderKey) match {
     case Some(temporaryUploadFolderValue) => temporaryUploadFolderValue
     case None => throw ApiConfigException(s"ApiConfig temporary upload folder information ${ApiConfig.TemporaryUploadFolderKey} missing")
-  }  
-  
-  
+  }
+
   /**
    * Used by Angular $logProvider service to enable or disable debug log.
    */
@@ -41,17 +39,16 @@ class ApiConfig {
     case Some(clientDebugEnabledValue) => clientDebugEnabledValue
     case None => throw ApiConfigException(s"ApiConfig client debug enabled information ${ApiConfig.ClientDebugEnabledUrlKey} missing")
   }
-  
+
   /**
    * Used by Api service to enable or disable queries cache feature.
-   * 
+   *
    * '''WARNING''': This feature is still in alpha state and should not be activated in production yet. Before production use it requires design review, extended testing and tuning (TTL,...) to make sure no unintended side effects exist.
    */
   val queryCacheEnabled: Boolean = Play.current.configuration.getBoolean(ApiConfig.QueryCacheEnabledUrlKey) match {
     case Some(queryCacheEnabledValue) => queryCacheEnabledValue
     case None => false // This property is optional, fallback to false without requiring configuration.
-  }    
-
+  }
 
 }
 
