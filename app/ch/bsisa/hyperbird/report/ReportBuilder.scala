d9320c71cc9b4e9f643e7381a942248af4fe323e
refond <refon@pobox.com>
2014-11-03 17:24:31 +0100

    Added comments, renamed variables.

--- app/ch/bsisa/hyperbird/report/ReportBuilder.scala
+++ app/ch/bsisa/hyperbird/report/ReportBuilder.scala
@@ -1,13 +1,13 @@
 package ch.bsisa.hyperbird.report
 
-import java.io.{InputStream}
+import java.io.{ InputStream }
 import java.net.URLEncoder
 import java.text.DecimalFormat
 import scala.concurrent.Future
 
-import ch.bsisa.hyperbird.dao.ws.{WSQueries, XQueryWSHelper}
+import ch.bsisa.hyperbird.dao.ws.{ WSQueries, XQueryWSHelper }
 import ch.bsisa.hyperbird.model.ELFIN
-import io.github.cloudify.scala.spdf.{Portrait, PdfConfig, Pdf}
+import io.github.cloudify.scala.spdf.{ Portrait, PdfConfig, Pdf }
 
 import org.apache.commons.codec.binary.Base64
 import play.api.Play
@@ -17,13 +17,11 @@ import play.api.templates.Template1
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-import scala.xml.{Elem, XML}
-
+import scala.xml.{ Elem, XML }
 
 object ReportBuilder {
 
-
-  def renderTemplate(templateName:String, xml:Elem):String = {
+  def renderTemplate(templateName: String, xml: Elem): String = {
     val ru = scala.reflect.runtime.universe
     val m = ru.runtimeMirror(getClass.getClassLoader)
     val template = m.reflectModule(m.staticModule(templateName + "$")).instance.asInstanceOf[Template1[Elem, Result]]
@@ -31,64 +29,71 @@ object ReportBuilder {
     template.render(xml).toString
   }
 
-  def writeReport(reportElfin:ELFIN)(implicit reportConfig:ReportConfig): Future[TemporaryFile] = {
+  def writeReport(reportElfin: ELFIN)(implicit reportConfig: ReportConfig): Future[TemporaryFile] = {
 
-
+    // ==============================================================
+    // Extract parameters from reportElfin
+    // ==============================================================    
     val headerTemplateName = reportElfin.CARACTERISTIQUE.get.CAR1.get.VALEUR.get
     val contentTemplateName = reportElfin.CARACTERISTIQUE.get.CAR2.get.VALEUR.get
     // URL encoding is necessary for query content but not for file name
     //val queryFileName = URLEncoder.encode(reportElfin.DIVERS.get.METHODE.get, "UTF-8")
     val queryFileName = reportElfin.DIVERS.get.METHODE.get
-    
-    // TODO: review if wrapped is what we want.
+
+    // ==============================================================
+    // Run XQuery by file name (TODO: review if wrapped is what we want.)
+    // ==============================================================
     val responseFuture = XQueryWSHelper.runWrappedXQueryFile(queryFileName.trim, None)
 
     responseFuture.map { response =>
       val resultData = XML.loadString(response.body)
-      val tempFile = new TemporaryFile(java.io.File.createTempFile("hb5ReportHeader", ".html"))
-      play.api.libs.Files.writeFile(tempFile.file, renderTemplate(headerTemplateName, resultData))
 
-      val page = renderTemplate(contentTemplateName, resultData)
+      // Render report header to HTML and save it to disk
+      val reportHeaderHtmlTempFile = new TemporaryFile(java.io.File.createTempFile("hb5ReportHeader", ".html"))
+      play.api.libs.Files.writeFile(reportHeaderHtmlTempFile.file, renderTemplate(headerTemplateName, resultData))
 
+      // Render report body to HTML and save it to disk
+      val reportContentHtmlString = renderTemplate(contentTemplateName, resultData)
+
+      // Configure wkhtmltopdf 
       val pdf = Pdf(
         reportConfig.wkhtmltopdfPath,
         new PdfConfig {
           orientation := Portrait
           pageSize := "A4"
-          headerHtml := tempFile.file.getAbsolutePath
+          headerHtml := reportHeaderHtmlTempFile.file.getAbsolutePath
 
-        }
-      )
-
+        })
+      // Create empty temporary file for final PDF report outcome.
       val tempResult = new TemporaryFile(java.io.File.createTempFile("hb5Report", ".pdf"))
+      // Process HTML temporary files to PDF using wkhtmltopdf
+      pdf.run(reportContentHtmlString, tempResult.file)
 
-      pdf.run(page, tempResult.file)
       tempResult
     }
   }
 
-  def toFormattedNumber(text:String):String = {
+  def toFormattedNumber(text: String): String = {
 
     try {
       val formatter = new DecimalFormat("###,###.##")
       val value = java.lang.Double.valueOf(text)
       formatter.format(value)
     } catch {
-      case _:Throwable => ""
+      case _: Throwable => ""
 
     }
 
   }
 
-
-  def encodeImage(path:String, mimeType:String): String = {
-    val isOption:Option[InputStream] = Play.current.resourceAsStream(path)
+  def encodeImage(path: String, mimeType: String): String = {
+    val isOption: Option[InputStream] = Play.current.resourceAsStream(path)
 
     isOption match {
       case Some(is) =>
         val bytes = new Array[Byte](is.available())
         is.read(bytes)
-        val base64:Base64 = new Base64()
+        val base64: Base64 = new Base64()
         // data:image/gif;base64,
         "data:" + mimeType + ";base64," + base64.encodeAsString(bytes)
 
