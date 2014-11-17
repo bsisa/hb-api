8a02cf4453b127d3fa8109b6ee82741b19d9613e
refond <refon@pobox.com>
2014-11-17 15:07:20 +0100

    Customizing SecureSocial Templates
    * Starting with copy from securesocial-2.1.3 default templates.
    * Created CustomTemplatesPlugin.
    * Configured play.plugins to make use of CustomTemplatesPlugin instead
      of DefaultTemplatesPlugin.

---
+++ app/ch/bsisa/hyperbird/security/securesocial/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains SecureSocial authentication module for Play Framework custom extensions implementations.  
+ * 
+ * @see http://securesocial.ws
+ * 
+ * @author Patrick Refondini
+ *
+ */
+package ch.bsisa.hyperbird.security.securesocial;
