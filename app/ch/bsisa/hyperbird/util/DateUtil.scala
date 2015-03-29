1bc4deea9ac7103edccd9aca527b28f29ad645fd
refond <refon@pobox.com>
2015-03-30 00:21:48 +0200

    Added new formatter for ISO with and without time zone. Added getHourMinuteSecond triplet for a given date.

--- app/ch/bsisa/hyperbird/util/DateUtil.scala
+++ app/ch/bsisa/hyperbird/util/DateUtil.scala
@@ -1,6 +1,8 @@
 package ch.bsisa.hyperbird.util
 
 import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 /**
  * Utility object aimed at centralising concerns with text to Date objects
  * used in geoXml.xsd related fields conversions.
@@ -10,13 +12,26 @@ import java.text.SimpleDateFormat
  * <i>Note: By default never ever let a java.text.DateFormat be lenient,
  * this is a terrible default behaviour.</i>
  *
+ * For date patterns check: http://docs.oracle.com/javase/6/docs/api/java/text/SimpleDateFormat.html
+ * 
  */
 object DateUtil {
-
+  
   /**
    * Constant simple date format pattern used for Id/ID_G date and time part.
    */
   private val DateFormatPattern = "yyyyMMddHHmmssSSS"
+
+  /**
+   * Constant for ISO date pattern with time zone
+   */
+  private val IsoDateFromatPatternWithTz = "yyyy-MM-dd'T'HH:mm:ssZ"
+
+  /**
+   * Constant for ISO date pattern without time zone. (Timezone like +01:00 are not supported only +0100)
+   * See: https://community.oracle.com/thread/2043704
+   */    
+  private val IsoDateFromatPatternWithoutTz = "yyyy-MM-dd'T'HH:mm:ss"
     
   /**
    * Constant simple date format pattern used for ELFIN.IDENTIFIANT.{DE,A,PAR} 
@@ -41,7 +56,27 @@ object DateUtil {
     sdf.setLenient(false)
     sdf
   }
+  
+  
+  /**
+   * Hb dates formatter
+   */
+  private val isoDateFormatterWithTz = { 
+    val sdf = new SimpleDateFormat(IsoDateFromatPatternWithTz)
+    sdf.setLenient(false)
+    sdf
+  }  
 
+  /**
+   * Hb dates formatter
+   */
+  private val isoDateFormatterWithoutTz = { 
+    val sdf = new SimpleDateFormat(IsoDateFromatPatternWithoutTz)
+    sdf.setLenient(false)
+    sdf
+  }    
+  
+  
   /**
    * Returns a date formatter for elfin.Id/ID_G.
    */
@@ -50,19 +85,41 @@ object DateUtil {
   }
 
   /**
-   * Returns a date formatter with pattern: "yyyyMMddHHmmssSSS"
+   * Returns a date formatter with pattern: `yyyyMMddHHmmssSSS`
    */
   val elfinIdentifiantDateFormat = {
     idsFormatter
   }
   
   /**
-   * Returns a date formatter with pattern: "yyyy-MM-dd"
+   * Returns a date formatter with pattern: `yyyy-MM-dd`
    */
   val hbDateFormat = {
     hbDateFormatter
   }  
   
+  /**
+   * Returns a date formatter with pattern: `yyyyMMdd'T'HH:mm:ssZ`
+   */
+  val isoWithTzDateFormat = {
+    isoDateFormatterWithTz
+  }  
   
+    /**
+   * Returns a date formatter with pattern: `yyyyMMdd'T'HH:mm:ssZ`
+   */
+  val isoWithoutTzDateFormat = {
+    isoDateFormatterWithoutTz
+  }  
 
+  /**
+   * Returns a triplet (hour24:Int, minute:Int, second:Int) for the given date
+   */
+  def getHourMinuteSecond(date: Date): (Int, Int, Int) = {
+    val calendar = Calendar.getInstance()
+    calendar.setTime(date)
+    // Hour in 24h format
+    (calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND))
+  }  
+  
 }
