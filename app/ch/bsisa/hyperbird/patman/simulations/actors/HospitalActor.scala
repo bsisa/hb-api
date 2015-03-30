b68470e3081ca71c4d8c43b9b7225377509c5381
refond <refon@pobox.com>
2015-03-30 12:27:08 +0200

    Make use of NextHospitalStatesRequest message and HospitalHelper.

--- app/ch/bsisa/hyperbird/patman/simulations/actors/HospitalActor.scala
+++ app/ch/bsisa/hyperbird/patman/simulations/actors/HospitalActor.scala
@@ -1,15 +1,19 @@
 package ch.bsisa.hyperbird.patman.simulations.actors
 import akka.actor.{Actor,ActorLogging}
 import ch.bsisa.hyperbird.patman.simulations.messages.HospitalState
+import ch.bsisa.hyperbird.patman.simulations.model.HospitalHelper
+import ch.bsisa.hyperbird.patman.simulations.messages.HospitalStatesRequest
+import ch.bsisa.hyperbird.patman.simulations.messages.NextHospitalStatesRequest
 
-class HospitalActor(name : String) extends Actor with ActorLogging {
-
+class HospitalActor(name : String, bedsNb : Int) extends Actor with ActorLogging {
+  
 	def receive = {
 	  case hospitalState: HospitalState => 
-	    log.info(s"HospitalActor(${name}) received new hospitalState '$hospitalState' from ${sender.path.name} of CLASSE = ${hospitalState.elfin.CLASSE} waiting for 5sec!!!")
-	    Thread.sleep(5000)
+	    log.info(s"HospitalActor(${name}) received new hospitalState schedule ${hospitalState.elfin.IDENTIFIANT.get.DE.get}")
+	    val hospital = HospitalHelper.toHospital(hospitalState.elfin)
+	    log.info("hospital: " + hospital)
 	    // Check state received hospital id matches our name otherwise cancel simulation!
-	    sender ! s"received hospitalState of CLASSE = ${hospitalState.elfin.CLASSE}"
+	    sender ! NextHospitalStatesRequest(name)
 	}
 	
 }
