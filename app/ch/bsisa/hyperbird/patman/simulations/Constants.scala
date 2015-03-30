2bae0b2ebdfed104604bc3ffa9e85a51ce5e17df
refond <refon@pobox.com>
2015-03-30 12:24:34 +0200

    Add Hospital, Bed models related constants.

--- app/ch/bsisa/hyperbird/patman/simulations/Constants.scala
+++ app/ch/bsisa/hyperbird/patman/simulations/Constants.scala
@@ -4,5 +4,15 @@ object Constants {
 
   val HOSPITAL_CODE_CDF = "cdf"
   val HOSPITAL_CODE_PRT = "prt"
-  
+
+  val BED_FREE_CODE = "libre"
+  val BED_BUSY_CODE = "occupé"
+
+  val PATIENT_TYPE_SI = "soins intensifs"
+  val PATIENT_TYPE_SC = "soins continus"
+
+  val TRANSFER_TYPE_SPEC = "spécialisé"
+  val TRANSFER_TYPE_MED = "médicalisé"
+  val TRANSFER_TYPE_NON_MED = "non-médicalisé"
+
 }
