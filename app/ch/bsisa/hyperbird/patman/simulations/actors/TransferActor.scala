abf3e0f78cca92818290847119e871107a752682
refond <refon@pobox.com>
2015-03-30 20:40:15 +0200

    Implement DataSetUpdateRequest, DataSetUpdateResponse.

--- app/ch/bsisa/hyperbird/patman/simulations/actors/TransferActor.scala
+++ app/ch/bsisa/hyperbird/patman/simulations/actors/TransferActor.scala
@@ -3,8 +3,10 @@ package ch.bsisa.hyperbird.patman.simulations.actors
 import akka.actor.{ Actor, ActorRef, ActorLogging }
 import ch.bsisa.hyperbird.patman.simulations.messages.TransferRequest
 import ch.bsisa.hyperbird.patman.simulations.messages.TransferResponse
+import ch.bsisa.hyperbird.patman.simulations.messages.DataSetUpdateRequest
+import ch.bsisa.hyperbird.patman.simulations.messages.DataSetUpdateResponse
 
-class TransferActor(hospitalsActorRefs: Map[String, ActorRef]) extends Actor with ActorLogging {
+class TransferActor(hospitalsActorRefs: Map[String, ActorRef], dataSetActorRef : ActorRef) extends Actor with ActorLogging {
 
   // Listen to request for transfer
   // Request hospital(s) to know whether the transfer is possible
@@ -21,6 +23,12 @@ class TransferActor(hospitalsActorRefs: Map[String, ActorRef]) extends Actor wit
       log.info(s"TransferResponse from ${fromHospitalCode} to ${toHospitalCode}")
       // Forward response to original requester (fromHospitalCode)
       hospitalsActorRefs(fromHospitalCode) ! TransferResponse(id, status, acceptedIncomingBeds, fromHospitalCode, toHospitalCode, fromSchedule, message)
+      
+    case DataSetUpdateRequest(allTransferredSiBeds, fromHospitalCode, toHospitalCode, fromSchedule) => 
+      dataSetActorRef ! DataSetUpdateRequest(allTransferredSiBeds, fromHospitalCode, toHospitalCode, fromSchedule)
+      
+    case DataSetUpdateResponse(status, fromHospitalCode, toHospitalCode, fromSchedule) => 
+      hospitalsActorRefs(fromHospitalCode) ! DataSetUpdateResponse(status, fromHospitalCode, toHospitalCode, fromSchedule)
   }
 
 }
