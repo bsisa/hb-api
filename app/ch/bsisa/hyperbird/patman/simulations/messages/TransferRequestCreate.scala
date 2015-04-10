4da965e67fd4633df3fc6a623ff089bad40c5359
refond <refon@pobox.com>
2015-04-10 17:26:28 +0200

    Added TODOs.

--- app/ch/bsisa/hyperbird/patman/simulations/messages/TransferRequestCreate.scala
+++ app/ch/bsisa/hyperbird/patman/simulations/messages/TransferRequestCreate.scala
@@ -3,6 +3,7 @@ package ch.bsisa.hyperbird.patman.simulations.messages
 import ch.bsisa.hyperbird.patman.simulations.model.Bed
 import java.util.Date
 
+// TODO: remove outgoingSiBeds. This is only meaningful to TransferRequestDelete
 case class TransferRequestCreate(id: String, incomingSiBeds: List[Bed], outgoingSiBeds: List[Bed], typeScToSiBeds: List[Bed], fromHospitalCode: String, toHospitalCode: String, fromSchedule: Date, message: String) {
 
 }
