f59300c8a2873f91c5d0e7b8fd63dacb02713405
refond <refon@pobox.com>
2015-04-24 18:37:44 +0200

    Code clean up.

--- app/ch/bsisa/hyperbird/patman/simulations/actors/TransferReportActor.scala
+++ app/ch/bsisa/hyperbird/patman/simulations/actors/TransferReportActor.scala
@@ -79,64 +79,6 @@ class TransferReportActor(simulationId: String) extends Actor with ActorLogging 
           case e: Throwable => log.error(s"TransferReportActor complaining: ${e}")
         }
       }
-      
-    case TransferRequest(id, incomingSiBeds, outgoingSiBeds, typeScToSiBeds, fromHospitalCode, toHospitalCode, fromSchedule, message) =>
-
-      log.info(s"TransferReportActor notified of transfer from ${fromHospitalCode} to ${toHospitalCode} at ${fromSchedule}")
-      //      val transferEvenElfin = ELFIN
-      //      ElfinDAO.create(elfin)
-      val futureTransferElfin: Future[ELFIN] = ElfinDAO.getNewFromCatalogue("TRANSFER")
-      futureTransferElfin.map { elfinTransferTemplate =>
-
-        try {
-
-          //log.info(s">>>> OBTAINED TRANSFER FROM CATALOGUE:\n ${elfinTransferTemplate}")
-
-          /**
-           * Create at least one at most two TRANSFER entries:
-           * 1) NATURE="add" for incomingSiBeds and typeScToSiBeds
-           * 2) NATURE="remove" for type outgoingSiBeds
-           * This to allow easy selection for reporting
-           */
-
-          // Compute list of beds to transfer (+)
-          val bedsToAdd = incomingSiBeds ++ typeScToSiBeds
-
-          if (bedsToAdd.size > 0) {
-            val addTransferElfinFuture = HospitalHelper.buildTransferElfin(
-              elfinTransferTemplate = elfinTransferTemplate, simulationId = simulationId, nature = Constants.TRANSFER_NATURE_ADD,
-              fromHospitalCode = fromHospitalCode, toHospitalCode = toHospitalCode, schedule = fromSchedule, beds = bedsToAdd)
-            // Update database 
-            addTransferElfinFuture.map { addTransferElfin =>
-              ElfinDAO.create(addTransferElfin)
-            }
-          }
-
-          // Compute list of beds to notify to remove (outgoing patients) (-)
-          val bedsToRemove = outgoingSiBeds
-
-          if (bedsToRemove.size > 0) {
-            val removeTransferElfinFuture = HospitalHelper.buildTransferElfin(
-              elfinTransferTemplate = elfinTransferTemplate, simulationId = simulationId, nature = Constants.TRANSFER_NATURE_REMOVE,
-              fromHospitalCode = fromHospitalCode, toHospitalCode = toHospitalCode, schedule = fromSchedule, beds = bedsToRemove)
-            // Update database 
-            removeTransferElfinFuture.map { removeTransferElfin =>
-              ElfinDAO.create(removeTransferElfin)
-            }
-          }
-
-          // TODO: we should also have beds to update for existing transfered beds which do have their 
-          // transfer type changed or (to review) SI to SC patient type change 
-
-        } catch {
-          case e: Throwable => log.error(s"TransferReportActor complaining: ${e}")
-        }
-
-      }
-
-//    case TransferResponse(id, status, acceptedIncomingBeds, fromHospitalCode, toHospitalCode, fromSchedule, message) =>
-//      log.info(s"TransferResponse id ${id} from ${fromHospitalCode} to ${toHospitalCode} status = ${status}")
-//    // We could write to database if status is not successful...
 
     case DataSetEmpty =>
       sender ! WorkCompleted("TransferReportActor")
