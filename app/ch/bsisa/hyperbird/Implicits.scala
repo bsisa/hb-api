e9d8cd8a94c72d15536371c8bf3d8b11300953e7
refond <refon@pobox.com>
2015-05-06 10:59:16 +0200

    Formatting.

--- app/ch/bsisa/hyperbird/Implicits.scala
+++ app/ch/bsisa/hyperbird/Implicits.scala
@@ -4,44 +4,39 @@ import ch.bsisa.hyperbird.dao.DbConfig
 import ch.bsisa.hyperbird.report.ReportConfig
 
 /**
-   * Configuration objects exposed as implicit values.
-   *
-   *
-   * To get access to these implicit values a class needs to declare:
-   * <code>import ch.bsisa.hyperbird.Implicits._</code>
-   * 
-   * @author Patrick Refondini
-   */
+ * Configuration objects exposed as implicit values.
+ *
+ *
+ * To get access to these implicit values a class needs to declare:
+ * <code>import ch.bsisa.hyperbird.Implicits._</code>
+ *
+ * @author Patrick Refondini
+ */
 object Implicits {
-
 
   /**
    * Database configurations
    */
   implicit val dbConfig = new DbConfig
 
-  
   /**
    * Database configurations
    */
   implicit val collectionsConfig = new CollectionsConfig
-  
-  
+
   /**
    * Init configurations
    */
   implicit val initConfig = new InitConfig
-  
-  
+
   /**
    * Api configurations
    */
   implicit val apiConfig = new ApiConfig
 
+  /**
+   * Report Configuration
+   */
+  implicit val reportConfig = new ReportConfig
 
-    /**
-     * Report Configuration
-     */
-    implicit val reportConfig = new ReportConfig
-  
 }
