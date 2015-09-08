5fc3f3af7e5b970c8effb47e66b16ac7bd8b9bb0
refond <refon@pobox.com>
2015-09-08 11:54:56 +0200

    Apache POI upgraded to latest stable release 3.12. Does not fix the LibreOffice editing proble introducing "Leftover xxx bytes" exception.

--- project/Build.scala
+++ project/Build.scala
@@ -14,8 +14,8 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     "io.github.cloudify" %% "spdf" % "1.3.1",
-    "org.apache.poi" % "poi" % "3.10-FINAL",
-    "org.apache.poi" % "poi-ooxml" % "3.10-FINAL",
+    "org.apache.poi" % "poi" % "3.12",
+    "org.apache.poi" % "poi-ooxml" % "3.12",
     "commons-io" % "commons-io" % "2.4",
     "org.jsoup" % "jsoup" % "1.7.3",
     "net.liftweb" %% "lift-json" % "2.5",
