923fb983570a0dd049c4f3b4743703863af5b2c4
refond <refon@pobox.com>
2015-09-15 15:56:36 +0200

    CFC codes CSV to ELFIN conversion. Required to produce data for orders_stat_module project data migration. Check hb-data db_migration procedure for orders_stat_module.

---
+++ test/test/ch/bsisa/hyperbird/util/CfcConvert.scala
@@ -0,0 +1,66 @@
+package test.ch.bsisa.hyperbird.util
+
+import ch.bsisa.hyperbird.model._
+import ch.bsisa.hyperbird.model.format.Implicits._
+import ch.bsisa.hyperbird.model.proto._
+import ch.bsisa.hyperbird.util.format.JsonXmlConverter
+import org.specs2.mutable._
+import scala.xml.XML
+import play.api.libs.json.Json
+import play.api.libs.json._
+
+import java.io.File
+import scala.io.Source
+
+/**
+ *
+ * Important: 
+ * 
+ * CFC codes cannot be padded with 0. These are not numbers.
+ * 
+ * For instance: 
+ * 0, 00, 000 are three valid and different codes
+ * 9, 009 are two valid and completely different codes...
+ *
+ * Tip: from sbt play console run:
+ * {{{
+ * test-only test.ch.bsisa.hyperbird.util.CfcConvert
+ * }}}
+ * to have only the current test run.
+ *
+ * @author Patrick Refondini
+ */
+class CfcConvert extends BaseSerialisationSpec {
+
+  val cfcFilePath = TestResourcesDir + "cfc.csv"
+  
+  var i = 0;
+  
+  Source.fromFile(new File(cfcFilePath), "UTF8").getLines.foreach { line =>
+    
+    i = i+1
+    val tokens: Array[String] = line.split(" ")
+
+    val code: String = tokens(0)
+    val id : String = "G20150915030000" + "%03d".format(i)  
+    val comment: String = tokens.drop(1).toList.mkString(" ")
+    
+    val cfcCodeElfin = <ELFIN ID_G="G20150910130000002" Id={id} CLASSE="CODE" GROUPE="CFC" NATURE="Classification" TYPE="ACTIVITE" SOURCE="">
+        <IDENTIFIANT>
+            <AUT>PRE</AUT><!-- Input by (automatic) -->
+            <NOM>{code}</NOM><!-- Alphanumeric code -->
+        </IDENTIFIANT>
+        <CARACTERISTIQUE/>
+        <PARTENAIRE/>
+        <ACTIVITE/>
+        <DIVERS>
+            <REMARQUE>{comment}</REMARQUE><!-- Description -->
+        </DIVERS>
+    </ELFIN>
+    
+
+    //println(cfcCodeElfin)
+    XML.save(TestResultsDir + "CODE"+id+".xml", cfcCodeElfin, "UTF8", false)
+  }
+
+}
