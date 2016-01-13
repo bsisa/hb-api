5cf4c1c61d63aa62d36b324b698142c5f684c160
refond <refon@pobox.com>
2016-01-13 17:04:48 +0100

    No logic computeOrderFigures first draft.

---
+++ app/ch/bsisa/hyperbird/orders/OrderUtil.scala
@@ -0,0 +1,23 @@
+package ch.bsisa.hyperbird.orders
+
+import play.api.Logger
+
+import ch.bsisa.hyperbird.model.CARACTERISTIQUE
+
+/**
+ * Object groupping simple functions dealing with Orders. 
+ * 
+ * @author Patrick Refondini
+ */
+object OrderUtil {
+  
+  /**
+   * Provides next order number.
+   */
+  def computeOrderFigures( carP : CARACTERISTIQUE) : CARACTERISTIQUE = {
+    
+    carP
+
+  } 
+  
+}
