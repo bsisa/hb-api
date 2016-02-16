5560bc9b14359bb300981201ca5eb932efd9e404
refond <refon@pobox.com>
2016-02-16 15:43:42 +0100

    New configurations necessary to new PDF merging tool.

--- app/ch/bsisa/hyperbird/report/ReportConfig.scala
+++ app/ch/bsisa/hyperbird/report/ReportConfig.scala
@@ -3,14 +3,32 @@ package ch.bsisa.hyperbird.report
 import play.api.Play
 
 class ReportConfig {
+  
   /**
-   * Wkhtmltopdf Binary Path
+   * Wkhtmltopdf binary path
    */
-  val wkhtmltopdfPath: String = Play.current.configuration.getString(ReportConfig.WkhtmltopdfPathKey) match {
+  lazy val wkhtmltopdfPath: String = Play.current.configuration.getString(ReportConfig.WkhtmltopdfPathKey) match {
     case Some(path) => path
-    case None => throw ReportConfigException(s"Report path identifier information ${ReportConfig.WkhtmltopdfPathKey} missing")
+    case None => throw ReportConfigException(s"Report HTML to PDF tool path configuration information ${ReportConfig.WkhtmltopdfPathKey} missing")
   }
 
+  /**
+   * PDF merging binary path
+   */
+  lazy val pdfMergingPath: String = Play.current.configuration.getString(ReportConfig.PdfMergingPathKey) match {
+    case Some(path) => path
+    case None => throw ReportConfigException(s"Report PDF merging tool path configuration information ${ReportConfig.PdfMergingPathKey} missing")
+  }  
+  
+  /**
+   * PDF merging binary command name
+   */
+  lazy val pdfMergingCommand: String = Play.current.configuration.getString(ReportConfig.PdfMergingCommandKey) match {
+    case Some(command) => command
+    case None => throw ReportConfigException(s"Report PDF merging tool command name configuration information ${ReportConfig.PdfMergingCommandKey} missing")
+  }    
+  
+  
 }
 
 /**
@@ -22,5 +40,7 @@ case class ReportConfigException(message: String = null, cause: Throwable = null
 object ReportConfig {
 
   private val WkhtmltopdfPathKey = "hb.report.wkhtmltopdf.path"
+  private val PdfMergingPathKey = "hb.report.pdfmerging.path"
+  private val PdfMergingCommandKey = "hb.report.pdfmerging.command"
 
 }
