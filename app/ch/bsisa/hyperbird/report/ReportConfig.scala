d1e77574455a5bce3ae313f1def136c8fe44db40
refond <refon@pobox.com>
2016-02-17 15:18:51 +0100

    Add new constants to document configuration found in XML report configuration files (ELFIN[@CLASSE='RAPPORT']).

--- app/ch/bsisa/hyperbird/report/ReportConfig.scala
+++ app/ch/bsisa/hyperbird/report/ReportConfig.scala
@@ -33,4 +33,37 @@ object ReportConfig {
   private val WkhtmltopdfPathKey = "hb.report.wkhtmltopdf.path"
   private val PdfMergingPathKey = "hb.report.pdfmerging.path"
 
+  /**
+   * Header message is an optional string message configured on a report configuration basis (static). 
+   * It is passed to configured header template as message template parameter. 
+   */
+  val CAR_NAME_HEADER_MESSAGE = "headerMessage"
+  
+
+  /**
+   * Page orientation is an optional string message configured on a report configuration. 
+   * It defines the report default page orientation. 
+   * If not available `portrait` layout is used. 
+   */
+  val CAR_NAME_PAGE_ORIENTATION = "pageOrientation"  
+  
+  /**
+   * Page orientation `landscape` is currently the only page orientation accepted value.
+   * Any other value will be considered `portrait` page orientation.
+   */
+  val CAR_VALUE_PAGE_ORIENTATION_LANDSCAPE = "landscape"  
+  
+  
+  /**
+   * PDF include first is a string containing a HB triplet uniquely identifying an ELFIN object (IDG/CLASS/Id)
+   * This object first ANNEX document in PDF format will be used for PDF merging at first position. 
+   */  
+  val CAR_NAME_PDF_INCLUDE_FIRST = "pdfIncludeFirst"
+  
+  /**
+   * PDF include last is a string containing a HB triplet uniquely identifying an ELFIN object (IDG/CLASS/Id)
+   * This object first ANNEX document in PDF format will be used for PDF merging at last position. 
+   */
+  val CAR_NAME_PDF_INCLUDE_LAST = "pdfIncludeLast"
+ 
 }
