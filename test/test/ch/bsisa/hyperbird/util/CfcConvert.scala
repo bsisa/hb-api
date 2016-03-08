78bb7bf94b4ad8f6783378ceb0c335d546fccd11
refond <refon@pobox.com>
2016-03-08 14:15:03 +0100

    Add basic spec test to CfcConvert procedure.

--- test/test/ch/bsisa/hyperbird/util/CfcConvert.scala
+++ test/test/ch/bsisa/hyperbird/util/CfcConvert.scala
@@ -14,11 +14,11 @@ import scala.io.Source
 
 /**
  *
- * Important: 
- * 
+ * Important:
+ *
  * CFC codes cannot be padded with 0. These are not numbers.
- * 
- * For instance: 
+ *
+ * For instance:
  * 0, 00, 000 are three valid and different codes
  * 9, 009 are two valid and completely different codes...
  *
@@ -33,36 +33,49 @@ import scala.io.Source
 class CfcConvert extends BaseSerialisationSpec {
 
   val cfcFilePath = TestResourcesDir + "cfc.csv"
-  
+
   var i = 0;
-  
-  Source.fromFile(new File(cfcFilePath), "UTF8").getLines.foreach { line =>
-    
-    i = i+1
-    
+
+  val csvCfcInputFile = new File(cfcFilePath)
+  val XmlOutputFilePrefix = "CODE"
+  val XmlOutputFileSuffix = ".xml"
+
+  Source.fromFile(csvCfcInputFile, "UTF8").getLines.foreach { line =>
+
+    i = i + 1
+
     val tokens: Array[String] = line.split(" ")
 
     val code: String = tokens(0)
     val formattedIndex = "%03d".format(i)
-    val id : String = "G20150915030000" + formattedIndex  
+    val id: String = "G20150915030000" + formattedIndex
     val comment: String = tokens.drop(1).toList.mkString(" ")
-    
-    val cfcCodeElfin = <ELFIN ID_G="G20150910130000002" Id={id} CLASSE="CODE" GROUPE="CFC" NATURE="Classification" TYPE="ACTIVITE" SOURCE="">
-        <IDENTIFIANT>
-            <AUT>PRE</AUT><!-- Input by (automatic) -->
-            <NOM>{code}</NOM><!-- Alphanumeric code -->
-        </IDENTIFIANT>
-        <CARACTERISTIQUE><CAR1 NOM="Position de tri" VALEUR={formattedIndex} /></CARACTERISTIQUE>
-        <PARTENAIRE/>
-        <ACTIVITE/>
-        <DIVERS>
-            <REMARQUE>{comment}</REMARQUE><!-- Description -->
-        </DIVERS>
-    </ELFIN>
-    
+
+    val cfcCodeElfin = <ELFIN ID_G="G20150910130000002" Id={ id } CLASSE="CODE" GROUPE="CFC" NATURE="Classification" TYPE="ACTIVITE" SOURCE="">
+                         <IDENTIFIANT>
+                           <AUT>PRE</AUT><!-- Input by (automatic) -->
+                           <NOM>{ code }</NOM><!-- Alphanumeric code -->
+                         </IDENTIFIANT>
+                         <CARACTERISTIQUE><CAR1 NOM="Position de tri" VALEUR={ formattedIndex }/></CARACTERISTIQUE>
+                         <PARTENAIRE/>
+                         <ACTIVITE/>
+                         <DIVERS>
+                           <REMARQUE>{ comment }</REMARQUE><!-- Description -->
+                         </DIVERS>
+                       </ELFIN>
+
+    val xmlCfcOutputFilePath = TestResultsDir + XmlOutputFilePrefix + id + XmlOutputFileSuffix
 
     //println(cfcCodeElfin)
-    XML.save(TestResultsDir + "CODE"+id+".xml", cfcCodeElfin, "UTF8", false)
+    XML.save(xmlCfcOutputFilePath, cfcCodeElfin, "UTF8", false)
+  }
+
+  s"The conversion of CSV input file ${csvCfcInputFile.getCanonicalFile} " should {
+    s"create 572 XML files" in {
+      val xmlFiles = new File(TestResultsDir)
+      val matchingXmlFiles = xmlFiles.listFiles.filter(file => (!file.isDirectory && file.getName.startsWith(XmlOutputFilePrefix) && file.getName.endsWith(XmlOutputFileSuffix)))
+      matchingXmlFiles.size must equalTo(572)
+    }
   }
 
 }
