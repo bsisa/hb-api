5505dbef70788c810bc227f8028fce153390f651
refond <refon@pobox.com>
2016-04-25 12:25:31 +0200

    Obtain test obj to build fleet from database with default position.

--- app/ch/bsisa/hyperbird/actview/actors/FleetActor.scala
+++ app/ch/bsisa/hyperbird/actview/actors/FleetActor.scala
@@ -6,9 +6,12 @@ package ch.bsisa.hyperbird.actview.actors
 import akka.actor.{ Actor, ActorRef, ActorLogging, Props }
 import akka.routing.BroadcastRouter
 import akka.routing.RoundRobinRouter
+
+import ch.bsisa.hyperbird.model._
+
 import play.api.Play.current
 import play.api.libs.concurrent.Akka
-import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.Future
 
 /**
  * @author Patrick Refondini
@@ -18,28 +21,60 @@ class FleetActor(name: String, colour: String) extends Actor with ActorLogging {
 
   import context._
 
-  var fleetOpt: Option[Vector[ActorRef]] = None
+  var fleetOpt: Option[Seq[ActorRef]] = None
 
-  
-  // TODO: load obj infor from database 
-  def createFleet(): Vector[ActorRef] = {
+  // TODO: load obj info from database 
+  def createFleet(): Future[Seq[ActorRef]] = {
+
+    // Hardcoded Test values
+    val buildingColId = "G20040930101030005"
+    val fountainColId = "G20040930101030004"
+    val buildingClass = "IMMEUBLE"
+    val fountainClass = "FONTAINE"    
     
-    val obj1ActName = s"${name}-1"
-    val obj2ActName = s"${name}-2"
-    val obj3ActName = s"${name}-3"
-    val obj1Act = actorOf(Props(new ObjectActor(objectId = obj1ActName)), name = obj1ActName)
-    val obj2Act = actorOf(Props(new ObjectActor(objectId = obj2ActName)), name = obj2ActName)
-    val obj3Act = actorOf(Props(new ObjectActor(objectId = obj3ActName)), name = obj3ActName)
+    val fountainsConfig = (fountainColId, fountainClass)
+    val buildingsConfig = (buildingColId, buildingClass)
+    
+    // Parameter
+    val configPair = buildingsConfig
 
-    Vector(obj1Act, obj2Act, obj3Act)
+    val objColId = configPair._1
+    val objClass = configPair._2
 
+    /*
+     * Example of produced XPath
+     * "//ELFIN[@CLASSE='IMMEUBLE' and IDENTIFIANT/OBJECTIF='195']" // //ELFIN[@CLASSE='IMMEUBLE' and FORME/POINT[@FONCTION='BASE']]
+     */
+    val xPath = s"//ELFIN[@CLASSE='$objClass' and FORME/POINT[@FONCTION='BASE']]"
+    
+    
+    // Make use of database API to obtain a set of objects with a start position 
+    import ch.bsisa.hyperbird.dao.ws.XQueryWSHelper
+    import ch.bsisa.hyperbird.dao.ws.WSQueries
+    import ch.bsisa.hyperbird.Implicits._
+
+    val elfinsFuture = XQueryWSHelper.queryElfins(WSQueries.filteredCollectionQuery(objColId, xPath))
+
+    val objectsIdPositionFut: Future[Seq[(String, POINT)]] = elfinsFuture.map { elfins =>
+      for (elfin <- elfins) yield ((elfin.Id, elfin.FORME.get.POINT(0)))
+    }
+
+    val actorRefSeqFut = objectsIdPositionFut.map { objectsIdPosition =>
+      val actorRefSeq = for (objectIdPosition <- objectsIdPosition) yield {
+        val objAct = actorOf(Props(new ObjectActor(objectId = objectIdPosition._1, startPosition = objectIdPosition._2)), name = objectIdPosition._1)
+        objAct
+      }
+      actorRefSeq
+    }
+    actorRefSeqFut
   }
 
   def receive = {
     case "start" =>
       log.info(s"Start $colour $name fleet")
-      //TODO: create a fleet of ObjectActor from a list of objects with position (IMM.)
-      fleetOpt = Some(createFleet())
+      createFleet().map { objActRefs =>
+        fleetOpt = Some(objActRefs)
+      }
 
     case "stop" =>
       log.info(s"Stop $colour $name fleet")
--- app/ch/bsisa/hyperbird/actview/actors/ObjectActor.scala
+++ app/ch/bsisa/hyperbird/actview/actors/ObjectActor.scala
@@ -11,16 +11,22 @@ import akka.actor.{ Actor, ActorLogging }
  * @author Patrick Refondini
  *
  */
-class ObjectActor(objectId:String) extends Actor with ActorLogging {
+class ObjectActor(objectId:String, startPosition:POINT) extends Actor with ActorLogging {
 
-  var position : Option[POINT] = None  
+  var position : POINT = startPosition  
   var destination : Option[POINT] = None
   
   def receive = {
     case message:String => 
       log.info(s"ObjectActor objectId=$objectId received message: $message")
+      // TODO: SetDestination(POINT(pos, Some(x), Some(y), z, ksi, angle, alpha, xs, ys, zs, ksis, angles, alphas, id, id_g, _ , objClass, group, remark))
     case POINT(pos, Some(x), Some(y), z, ksi, angle, alpha, xs, ys, zs, ksis, angles, alphas, id, id_g, _ , objClass, group, remark) => 
       log.info(s"ObjectActor objectId=$objectId received new position (x,y,z) = ($x, $y, $z)")
+    // TODO: GetDestination
+    //case
+    // TODO: GetPosition
+    //case 
+    
     case _ => log.warning("Received message other than expected String...")
   }
   
